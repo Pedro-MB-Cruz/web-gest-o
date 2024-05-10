3e819cfc91e90ce3f3c9c4637149977ed9555598
Diogo Castro <diogocastro.dev@gmail.com>
2024-05-10 12:18:06 +0100

    Backup 10/04 (Não deve estar completo)

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,38 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import router from "./router/index.route";
+import cookieParser from "cookie-parser";
+import session from "express-session";
 
-const app = express();
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(router);
+declare module "express-session" {
+  interface SessionData {
+    logged_in: boolean;
+    user: {
+      id: number;
+      username: string;
+      admin: boolean;
+    };
+  }
+}
 
-const port = process.env.PORT || 3000;
+const app = express(); // Create express app
+app.use(morgan("tiny")); // Log HTTP requests
+app.use(cors()); // Enable CORS
+app.use(cookieParser()); // Parse cookies
+const sess = {
+  secret: process.env.SESSION_SECRET || "secret", // Secret key for session
+  cookie: {
+    secure: false, // HTTP only (true for HTTPS)
+    maxAge: 1000 * 60 * 60 * 24, // 24 hours
+  },
+  resave: false, // Save session only if it's modified
+  saveUninitialized: true, // Create session if it's new
+};
+app.use(session(sess)); // Enable session
+app.use(router); // Use routers
+
+const port = process.env.PORT || 3000; // Read port from .env or default port is 3000
 app.listen(port, () => {
+  // Start server
   console.log(`🚀 Server is running on http://localhost:${port}`);
 });
