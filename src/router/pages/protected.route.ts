6d34640d8e66b8aaf38ef02cc2e089affc94627a
Pedro-MB-Cruz <pedrombcruz@outlook.com>
2024-06-19 10:37:53 +0100

    rotas

--- src/router/pages/protected.route.ts
+++ src/router/pages/protected.route.ts
@@ -2,6 +2,12 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get("/", (req,res)=>{
+  res.sendFile("pages/firstpage.html",{
+    root:".",
+  });
+});
+
 router.get("/admin", (req, res) => {
   res.sendFile("pages/firstpageadmin.html", {
     root: ".",
